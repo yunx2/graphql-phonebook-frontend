49ffd7fd399e3ab26700ccd067c3c97ea1245c2c
Yunyun <celeph4is@gmail.com>
2021-02-06 22:46:25 -0800

    code-a-long up to 'named queries and variables'; currently works with backend

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { gql, useQuery } from '@apollo/client';
+
+import Persons from './Persons';
+
+const ALL_PERSONS = gql`
+query {
+  allPersons  {
+    name
+    phone
+    id
+  }
+}
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  return (
+    <Persons persons = {result.data.allPersons}/>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ApolloClient, HttpLink, InMemoryCache, gql, ApolloProvider } from '@apollo/client'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const query = gql`
+query {
+  allPersons  {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
